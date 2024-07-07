fa2c3b5bd3b38b756233b3c8573cda11f7368cc9
Josh Hannaford <hannaford.joshua55@gmail.com>
2024-07-06 22:23:55 -0400

    Add correct move semantics for InfixExpression

--- src/ast/infix_expression.cc
+++ src/ast/infix_expression.cc
@@ -1,11 +1,13 @@
 #include "infix_expression.h"
 
+#include <utility>
+
 namespace ast {
 
 InfixExpression::InfixExpression(Token token, std::string op,
                                  std::unique_ptr<Expression>&& left,
                                  std::unique_ptr<Expression>&& right)
-    : m_token{token}, m_op{std::move(op)}, m_left{std::move(left)},
+    : m_token{std::move(token)}, m_op{std::move(op)}, m_left{std::move(left)},
       m_right{std::move(right)} {}
 
 auto InfixExpression::token_literal() const -> std::string {
